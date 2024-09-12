20c2f7dc4e373464fc16ae145797e52666e078bd
PeaChack <dev.peachack@gmail.com>
2024-09-12 22:16:12 +0300

    commit all changes

---
+++ recommendation-service/src/test/java/by/peachack/recommendationservice/RecommendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.peachack.recommendationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
