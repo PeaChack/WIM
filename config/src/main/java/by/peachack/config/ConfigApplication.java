1cbc1e40376461736047ce5efabd9f4de47b8740
d_bulyga <d.bulyga@btslogistics.by>
2025-03-11 12:49:47 +0300

    some changes

--- config/src/main/java/by/peachack/config/ConfigApplication.java
+++ config/src/main/java/by/peachack/config/ConfigApplication.java
@@ -2,10 +2,10 @@ package by.peachack.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigurationProperties
+@EnableConfigServer
 public class ConfigApplication {
 
     public static void main(String[] args) {
